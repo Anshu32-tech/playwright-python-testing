f48c4ee8e98b3de1e691208ddd5e7ee6062e04e0
Akshada <161299553+Akshada2906@users.noreply.github.com>
2026-09-04 11:26:50 +0530

    Refactor test to use class-based structure

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,6 +1,6 @@
-
-def test_verify_user_login_C46():
-    print("Navigating to /login")
-    print("Entering username admin")
-    print("Clicking login button")
-    assert True
+class Test_Login:
+    def test_verify_user_login_C46(self):
+        print("Navigating to /login")
+        print("Entering username admin")
+        print("Clicking login button")
+        assert True
